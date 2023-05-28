6feb5b56d4ebe92509c323018c742d4a936ef52c
Yash Pandey <pandeyyash7052@gmail.com>
2023-05-28 11:53:26 +0530

    updated home page and sidebar text-color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import SocialLinks from "../components/SocialLinks"
 import { NavLink } from "react-router-dom"
 
 const Home = () => {
-    
+
     return (
         <div
             name="Home"
@@ -12,16 +12,17 @@ const Home = () => {
             <div className="max-w-screen-lg  mx-auto flex flex-col justify-center w-full h-full gap-6">
 
 
-                <span className="mt-20 text-4xl">
-                Bonjour !..  <span>
-                I am <NavLink to="/" className="text-yellow-200 font-medium"> Yash Pandey</NavLink>  </span> 
+                <span className="mt-20 text-4xl flex flex-col gap-4 md:flex-row">
+                    <span>Bonjour !</span> 
+                <span>
+                        I am <NavLink to="/" className="text-yellow-200 font-medium"> Yash Pandey</NavLink>  </span>
                 </span>
                 <div className="Line-break "></div>
                 <p className=" text-xl md:text-2xl  text-slate-300">
-                    Working with my hands to make magic happen on the internet. View my <NavLink to="/projects" className="text-yellow-200 font-medium">Projects</NavLink> , Resume, <NavLink  className="text-yellow-200 font-medium" to="/contact" >Contact Me</NavLink> , or send me an email at <NavLink  className="text-yellow-200 font-medium" to="https://mail.google.com/mail/">pandeyyash7052@gmail.com</NavLink>.
+                    Working with my hands to make magic happen on the internet. View my <NavLink to="/projects" className="text-yellow-200 font-medium">Projects</NavLink> , Resume, <NavLink className="text-yellow-200 font-medium" to="/contact" >Contact Me</NavLink> , or send me an email at <NavLink className="text-yellow-200 font-medium" to="https://mail.google.com/mail/">pandeyyash7052@gmail.com</NavLink>.
                 </p>
                 <h1 className="text-md">Know more about me </h1>
-               <SocialLinks />
+                <SocialLinks />
             </div>
         </div>
     )
--- src/components/MenuBar/Menuitems.jsx
+++ src/components/MenuBar/Menuitems.jsx
@@ -6,15 +6,15 @@ const Menuitems = ({ state, handleClick }) => {
   return (
     <div>
       {state ? (
-        <ul className={`flex-col flex items-center fixed inset-0 left-1/4 uppercase bg-black/30 backdrop-blur-md gap-8 justify-center p-8  duration-200 `}>
+        <ul className={`flex-col  flex items-center fixed inset-0 left-1/4 uppercase bg-black/30 backdrop-blur-md gap-8 justify-center p-8  duration-200 `}>
           <button onClick={handleClick} className="absolute top-5 right-3 m-2">
             <CloseIcon />
           </button>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/about'>About</Link></li>
-          <li><Link to='/contact'>Contact</Link></li>
-          <li><Link to='/projects'>Projects</Link></li>
-          <li><Link to='/skills'>Skills</Link></li>
+          <li><Link className="text-yellow-200" to='/'>Home</Link></li>
+          <li><Link className="text-yellow-200" to='/about'>About</Link></li>
+          <li><Link className="text-yellow-200" to='/contact'>Contact</Link></li>
+          <li><Link className="text-yellow-200" to='/projects'>Projects</Link></li>
+          <li><Link  className="text-yellow-200" to='/skills'>Skills</Link></li>
         </ul>
       ) : (
         <></>
